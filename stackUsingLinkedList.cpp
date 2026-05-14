31b7fdc9bb1245e9ad0efb05697f0e13f036adee
baihaqiramadhani-11 <baihakiramadhani66@gmail.com>
2026-05-14 18:04:12 +0700

    menambahkan node baru di bagian depan stack/linked list.

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -24,4 +24,12 @@ public:
     {
         top = NULL;
     }
+    int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout
+    }
 }
